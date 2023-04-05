4891bb443d9ab2bbe7000179782785706630e250
Oduntan Murtadha <muridel199@gmail.com>
2023-04-05 17:12:03 +0000

    create models

---
+++ models/murtadha_analytics/agg_public_holiday.sql
@@ -0,0 +1,8 @@
+/*
+1. We want to obtain the following insights from the data:
+The total number of orders placed on a public holiday every month for the past year'
+A public holiday is a day with a day_of_the_week number in the range 1 - 5 and a working_day value of false. 
+After your transformation, the derived table agg_public_holiday should be loaded 
+into the {your_id}_analytics schema, using the below table
+schema
+*/
