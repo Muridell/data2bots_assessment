04376e5ce89f16045cc390cc773d85aa00a53ded
Oduntan Murtadha <muridel199@gmail.com>
2023-04-06 17:51:39 +0000

    update agg_public_holiday model

---
+++ models/murtadha_analytics/best_performing_product.sql
@@ -0,0 +1,6 @@
+/*4. The product with the highest reviews , the day it was ordered the most, either that day was a 
+public holiday,  total review points, percentage distribution of the review points , 
+and percentage distribution of early shipments to late shipments for that particular product.
+
+All ingestion_date are the current date the table was generated
+*/
